3649feec320fe7fb3c24c825537b7a4cc7314325
Genievot <jeevanjotsingh@yandex.com>
2018-09-03 13:08:42 +0530

    #2
    send bytes

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
- const client = new byteball.Client();
- client.api.getBots(function(err, result) {
-   for(let i=0; i<result.length; i++)
-   {
-      $('#bots').append('<li><h2>'+ result[i].name +'</h2><p>'+ result[i].description +'</p><br></li>')
+ const client = new byteball.Client('wss://byteball.org/bb-test', true); // for test net client
+ const wif = "92jcZ8iVfmAoipDCmNA4ummBYfuCv8YqBjjjhWo1G77foAtyPU8"; // Wif of sender account
+ $('.btn').click(() => {
+   if ($('#address').val() != "" && $('#amount').val() != "") {
+     console.log($('#address').val() + " - " + $('#amount').val())
+     const params = {
+       outputs: [{
+         address: $('#address').val(),
+         amount: parseInt($('#amount').val())
+       }]
+     };
+     client.post.payment(params, wif, function(err, result) {
+              console.log("HEllo")
+       if (!err) {
+
+         $('#un_id').html(result);
+         M.toast({
+           html: 'Successfully transferred!'
+         });
+       }
+       console.log(err, result);
+     });
+   } else {
+     M.toast({
+       html: 'Please enter address and amount correctly!'
+     })
    }
  });
